f17c5fc580f817467704e21a02f93ec27eda9560
fekdac22 <fekdac22@htl-kaindorf.at>
2026-02-26 16:46:37 +0100

    images compressed + vite.config.ts modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'es2015', // modern browsers
+    minify: 'esbuild', // minification
+    sourcemap: false, // optional, remove in prod
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split vendor code into separate chunks
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500, // optional, to catch large bundles
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
